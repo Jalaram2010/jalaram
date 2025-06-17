b78e828811b916f2aa7919d3acde4bb3c21fad9b
Jalaram2010 <jalaram20072005@gmail.com>
2025-06-17 14:06:55 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,34 @@
+                            /* for loop */
+
+
+let i;
+
+for(i = 1;i <= 5;i++)
+{
+    console.log("*");
+}
+
+                        
+                         /* while loop */
+                         // iterations unkown by user
+ 
+let i = 1;
+
+while(i <= 5)
+{
+   console.log("*");
+   i++
+}
+
+
+                            /* do while loop */
+                            //iteration can be done in once
+
+
+let i = 1;
+
+do
+{
+   console.log("*");
+   i++
+}while(i <= 5)
